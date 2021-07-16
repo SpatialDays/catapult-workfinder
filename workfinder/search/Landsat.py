540a1a4a1a54d6e3191a40bf49530f23e7ffa78e
Emily <emily.selwood@sa.catapult.org.uk>
2021-07-16 09:31:39 +0100

    landsat 8 workings

--- workfinder/search/Landsat.py
+++ workfinder/search/Landsat.py
@@ -1,19 +1,26 @@
+import json
 import logging
-import os
-from pathlib import Path
 
 import pandas as pd
 import geopandas as gpd
+from libcatapult.queues.base_queue import BaseQueue
 
-from workfinder import get_config
-from workfinder.search import get_ard_list, get_redis_connection, get_aoi, get_ancillary_dir, download_ancillary_file, \
-    download_ancillary_http
+from workfinder import get_config, S3Api
+from workfinder.search import get_ard_list, get_aoi, download_ancillary_file, download_ancillary_http
 from workfinder.search.BaseWorkFinder import BaseWorkFinder
 
 
 class Landsat8 (BaseWorkFinder):
+
+    def __init__(self, s3: S3Api, redis: BaseQueue):
+        super().__init__()
+        self._s3 = s3
+        self._redis = redis
+
     def find_work_list(self):
-        rows_df = _get_rows_paths()
+        self._s3.get_s3_connection()
+
+        rows_df = self._get_rows_paths()
         df = _download_metadata()
         logging.info("finding scenes")
         df = df[df['WRS Path'].isin(rows_df['ROW'].values) & df['WRS Path'].isin(rows_df['PATH'].values)]
@@ -24,42 +31,45 @@ class Landsat8 (BaseWorkFinder):
 
     def find_already_done_list(self):
         region = get_config("app", "region")
-        return get_ard_list(f"common_sensing/{region.lower()}/landsat_8/")
+        return get_ard_list(self._s3, f"common_sensing/{region.lower()}/landsat_8/")
 
     def submit_tasks(self, to_do_list):
-        channel = get_config("landsat8", "redis_channel")
-        # get redis connection
-        conn = get_redis_connection()
-        # submit each task.
-        for index, e in to_do_list.iterrows():
-            payload = {
-                "in_scene": e['id'],
-                "s3_bucket": "public-eo-data",
-                "s3_dir": "test/landsat_8/",
-            }
-            conn.publish(channel, payload)
+        if to_do_list is not None and len(to_do_list) > 0:
+            channel = get_config("landsat8", "redis_channel")
+            # get redis connection
+            self._redis.connect()
+            # submit each task.
+            for index, e in to_do_list.iterrows():
+                payload = {
+                    "in_scene": e['id'],
+                    "s3_bucket": "public-eo-data",
+                    "s3_dir": "test/landsat_8/",
+                }
+                self._redis.publish(channel, json.dumps(payload))
+            self._redis.close()
+
+    def _get_rows_paths(self):
+        region = get_config("app", "region")
+        aoi = get_aoi(self._s3, region)
+        file_path = download_ancillary_file(self._s3, "WRS2_descending.geojson",
+                                            "SatelliteSceneTiles/landsat_pr/WRS2_descending.geojson")
+        world_granules = gpd.read_file(file_path)
+        # Create bool for intersection between any tiles - should try inversion to speed up...
+        world_granules[region] = world_granules.geometry.apply(lambda x: gpd.GeoSeries(x).intersects(aoi))
+        # Filter based on any True intersections
+        world_granules[region] = world_granules[world_granules[region]].any(1)
+        region_ls_grans = world_granules[world_granules[region] == True]
+        return region_ls_grans
 
 
 def _order_products(to_do_list):
-
-
-
-def _get_rows_paths():
-    region = get_config("app", "region")
-    aoi = get_aoi()
-    file_path = download_ancillary_file("WRS2_descending.geojson", "SatelliteSceneTiles/landsat_pr/WRS2_descending.geojson")
-    world_granules = gpd.read_file(file_path)
-    # Create bool for intersection between any tiles - should try inversion to speed up...
-    world_granules[region] = world_granules.geometry.apply(lambda x: gpd.GeoSeries(x).intersects(aoi))
-    # Filter based on any True intersections
-    world_granules[region] = world_granules[world_granules[region]].any(1)
-    region_ls_grans = world_granules[world_granules[region] == True]
-    return region_ls_grans
+    pass
 
 
 def _download_metadata():
     logging.info("downloading landsat8 metadata")
-    file_path = download_ancillary_http("LANDSAT_OT_C2_L2.csv.gz", "https://landsat.usgs.gov/landsat/metadata_service/bulk_metadata_files/LANDSAT_OT_C2_L2.csv.gz")
+    file_path = download_ancillary_http("LANDSAT_OT_C2_L2.csv.gz",
+                                        "https://landsat.usgs.gov/landsat/metadata_service/bulk_metadata_files/LANDSAT_OT_C2_L2.csv.gz")
     df = pd.read_csv(file_path)
     logging.info(f"got metadata: {df.size}")
     return df
--- workfinder/workfinder.py
+++ workfinder/workfinder.py
@@ -2,6 +2,8 @@
 import logging
 import sys
 
+import click
+
 from workfinder.search.Landsat import Landsat8
 from workfinder import get_default_s3_api, get_default_nats_api, get_default_redis_api, get_default_esa_api
 from workfinder.search.s1 import S1
@@ -13,32 +15,26 @@ from workfinder.search.s1_ard import S1ARD
 processors = {
     "S1": S1(get_default_s3_api(), get_default_redis_api(), get_default_esa_api()),
     "S1_ARD": S1ARD(get_default_s3_api(), get_default_nats_api()),
-    "LANDSAT8": Landsat8(),
+    "LANDSAT8": Landsat8(get_default_s3_api(), get_default_redis_api()),
 }
 
 
-def main():
-    if len(sys.argv) > 1:
-        param = sys.argv[1]
+@click.command()
+@click.option('--limit', default=-1, help='Number records to process', )
+@click.argument("process")
+def main(process, limit):
 
-        limit = -1
-        if len(sys.argv)> 2:
-            limit = int(sys.argv[2])
-
-        if param:
-            param = param.upper()
-            if processors[param]:
-                logging.info(f"starting work search for {param}")
-                work = processors[param].find_new_work()
-                if limit > 0:
-                    processors[param].submit_tasks(work.head(limit))
-                else:
-                    processors[param].submit_tasks(work)
-                logging.info(f"done work search for {param}")
-            else:
-                print(f"unknown processor type {param}")
+    param = process.upper()
+    if param in processors:
+        logging.info(f"starting work search for {param}")
+        work = processors[param].find_new_work()
+        if limit > 0:
+            processors[param].submit_tasks(work.head(limit))
+        else:
+            processors[param].submit_tasks(work)
+        logging.info(f"done work search for {param}")
     else:
-        print(f"not enough arguments")
+        print(f"unknown processor type {param}")
 
 
 if __name__ == '__main__':
