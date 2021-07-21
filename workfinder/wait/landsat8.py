22b97747682a165ddf01301d68ac7f4ab9ca9cb5
Emily Selwood <emilywselwood@gmail.com>
2021-07-21 16:21:35 +0100

    fix region

--- workfinder/wait/landsat8.py
+++ workfinder/wait/landsat8.py
@@ -24,24 +24,29 @@ class Landsat8(BaseWaiter):
         resp = self.espa.call(f"item-status/{order_details['order_id']}", body={'status': 'complete'})
         target_queue = get_config("landsat8", "redis_channel")
         target_bucket = get_config("AWS", "bucket")
-        for item in resp[order_id]:
-            url = item.get('product_dload_url')
-           landsat_registry = {
+        region = get_config("app", "region")
+        
+        landsat_registry = {
                 'LE04': 'landsat_7',
                 'LE05': 'landsat_5',
                 'LE07': 'landsat_7',
                 'LE08': 'landsat_8',
-            }
-            for k, v in landsat_registry.items():
-                if k in url:
-                    nm = v
-            if not nm:
-                raise Exception("unknown landsat url")
-            # build payload object
-            # TODO: this s3_dir should to be configurable
-            payload = {"in_scene": url, "s3_bucket": target_bucket, "s3_dir": f"common_sensing/{orderref}/{nm}/"}
-            # send to redis
-            self.q.publish(target_queue, json.dumps(payload))
+             }
+        
+        for item in resp[order_id]:
+            url = item.get('product_dload_url')
+            
+             for k, v in landsat_registry.items():
+                 if k in url:
+                     nm = v
+                     break
+             if not nm:
+                 raise Exception("unknown landsat url")
+             # build payload object
+             # TODO: this s3_dir should to be configurable
+             payload = {"in_scene": url, "s3_bucket": target_bucket, "s3_dir": f"common_sensing/{region}/{nm}/"}
+             # send to redis
+             self.q.publish(target_queue, json.dumps(payload))
 
     def get_redis_source_queue(self):
         return get_config("landsat8", "redis_pending_channel")
