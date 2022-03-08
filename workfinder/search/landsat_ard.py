e75d6b4c61a6161f94dcbf831db38e34b5e48ccc
Emily Selwood <emily.selwood@sa.catapult.org.uk>
2022-03-08 14:30:20 +0000

    add old landsat and mlwater search tools

--- workfinder/search/landsat_ard.py
+++ workfinder/search/landsat_ard.py
@@ -14,5 +14,41 @@ class Landsat8ARD (BaseArdWorkFinder):
         return "landsat_8"
 
     def get_stac_key(self):
-        return get_config("landsat_ard", "stac_collection_path")
+        return get_config("landsat_ard", "stac_collection_path_8")
+
+
+class Landsat7ARD (BaseArdWorkFinder):
+
+    def __init__(self, s3: S3Api, nats: NatsQueue):
+        super().__init__(s3, nats)
+
+    def get_sensor_name(self):
+        return "landsat_7"
+
+    def get_stac_key(self):
+        return get_config("landsat_ard", "stac_collection_path_7")
+
+
+class Landsat5ARD (BaseArdWorkFinder):
+
+    def __init__(self, s3: S3Api, nats: NatsQueue):
+        super().__init__(s3, nats)
+
+    def get_sensor_name(self):
+        return "landsat_5"
+
+    def get_stac_key(self):
+        return get_config("landsat_ard", "stac_collection_path_5")
+
+
+class Landsat4ARD (BaseArdWorkFinder):
+
+    def __init__(self, s3: S3Api, nats: NatsQueue):
+        super().__init__(s3, nats)
+
+    def get_sensor_name(self):
+        return "landsat_4"
+
+    def get_stac_key(self):
+        return get_config("landsat_ard", "stac_collection_path_4")
 
--- workfinder/workfinder.py
+++ workfinder/workfinder.py
@@ -9,20 +9,31 @@ from workfinder.search.Landsat import Landsat8
 from workfinder import default_s3_api, default_nats_api, default_redis_api, default_esa_api, \
     default_espa_api
 
-from workfinder.search.landsat_ard import Landsat8ARD
+from workfinder.search.landsat_ard import Landsat8ARD, Landsat4ARD, Landsat5ARD, Landsat7ARD
+from workfinder.search.mlwater_ard import Landsat4MlWaterArd, Landsat8MlWaterArd, Landsat7MlWaterArd, \
+    Landsat5MlWaterArd, Sentinel1MlWaterArd, Sentinel2MlWaterArd
 from workfinder.search.s1 import S1
 from workfinder.search.s1_ard import S1ARD
+from workfinder.search.s2_ard import S2ARD
 
 # Mapping of processor names to objects.
 # Note: Names must be upper case.
-from workfinder.search.s2_ard import S2ARD
-
 processors = {
     "S1": S1(default_s3_api(), default_redis_api(), default_esa_api()),
     "S1_ARD": S1ARD(default_s3_api(), default_nats_api()),
     "S2_ARD": S2ARD(default_s3_api(), default_nats_api()),
     "LANDSAT8": Landsat8(default_s3_api(), default_redis_api(), default_espa_api()),
     "LANDSAT8_ARD": Landsat8ARD(default_s3_api(), default_nats_api()),
+    "LANDSAT7_ARD": Landsat7ARD(default_s3_api(), default_nats_api()),
+    "LANDSAT5_ARD": Landsat5ARD(default_s3_api(), default_nats_api()),
+    "LANDSAT4_ARD": Landsat4ARD(default_s3_api(), default_nats_api()),
+    "LANDSAT8_MLWATER": Landsat8MlWaterArd(default_s3_api(), default_nats_api()),
+    "LANDSAT7_MLWATER": Landsat7MlWaterArd(default_s3_api(), default_nats_api()),
+    "LANDSAT5_MLWATER": Landsat5MlWaterArd(default_s3_api(), default_nats_api()),
+    "LANDSAT4_MLWATER": Landsat4MlWaterArd(default_s3_api(), default_nats_api()),
+    "S2_MLWATER": Sentinel2MlWaterArd(default_s3_api(), default_nats_api()),
+    "S1_MLWATER": Sentinel1MlWaterArd(default_s3_api(), default_nats_api()),
+
 }
 
 
