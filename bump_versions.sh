a03b56796fc361e6ec6bb88b556c671c238b70b4
Emily Selwood <emily.selwood@sa.catapult.org.uk>
2022-03-29 16:09:49 +0100

    fix aoi format

--- bump_versions.sh
+++ bump_versions.sh
@@ -5,3 +5,7 @@ NEW_VERSION=$2
 
 sed -i s/${OLD_VERSION}/${NEW_VERSION}/g k8s/*.yaml
 sed -i s/${OLD_VERSION}/${NEW_VERSION}/g setup.py
+
+git checkout -b "release-${NEW_VERSION}"
+git commit -am "bump versions"
+git push -u origin "release-${NEW_VERSION}"
