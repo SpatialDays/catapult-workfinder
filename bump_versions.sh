2f4f0762bfc520f141b85a10b1c225d2c0472697
Emily Selwood <emily.selwood@sa.catapult.org.uk>
2022-05-12 09:01:49 +0100

    bump versions

--- bump_versions.sh
+++ bump_versions.sh
@@ -4,6 +4,7 @@ OLD_VERSION=$1
 NEW_VERSION=$2
 
 sed -i s/${OLD_VERSION}/${NEW_VERSION}/g k8s/*.yaml
+sed -i s/${OLD_VERSION}/${NEW_VERSION}/g charts/workfinder/values.yaml
 sed -i s/${OLD_VERSION}/${NEW_VERSION}/g setup.py
 
 git checkout -b "release-${NEW_VERSION}"
