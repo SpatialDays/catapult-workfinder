86516008d5db1f8f347ec45af2c2c20032116658
Ivica <ivica-matic@outlook.com>
2023-01-20 14:27:59 +0000

    added granule filtering back in

--- workfinder/search/s2.py
+++ workfinder/search/s2.py
@@ -6,6 +6,7 @@ import pandas as pd
 from libcatapult.queues.base_queue import BaseQueue
 from sentinelsat import SentinelAPI
 import logging
+
 logger = logging.getLogger(__name__)
 
 from workfinder import get_config, S3Api
@@ -28,24 +29,25 @@ class S2(BaseWorkFinder):
         # aoi = get_aoi(self._s3, region)
         # logger.info(f"AOI for esa lookup is: {aoi}")
         # logger.info(f"Typeof aoi: {type(aoi)}")
-        aoi_str = """MULTIPOLYGON (((-175 -12,-179.99999 -12,-179.99999 -20,-175 -20,-175 -12)), ((175 -12,179.99999 -12,179.99999 -20,175 -20,175 -12)))"""
+        aoi_str = """MULTIPOLYGON (((-175 -12,-179.99999 -12,-179.99999 -20,-175 -20,-175 -12)), ((175 -12,179.99999
+        -12,179.99999 -20,175 -20,175 -12))) """
         aoi = loads(aoi_str)
         logger.info("Querying ESA API for Sentinel 2 data")
 
-        # world_granules = get_gpd_file(self._s3,
-        #                               "sentinel2_tiles_world.geojson",
-        #                               "SatelliteSceneTiles/sentinel2_tiles_world/sentinel2_tiles_world.geojson")
-        # # Create bool for intersection between any tiles - should try inversion to speed up...
-        # world_granules[region] = world_granules.geometry.apply(lambda x: gpd.GeoSeries(x).intersects(aoi))
-        # # Filter based on any True intersections
-        # world_granules[region] = world_granules[world_granules[region]].any(1)
-        # region_s2_grans = world_granules[world_granules[region] == True]
+        world_granules = get_gpd_file(self._s3,
+                                      "sentinel2_tiles_world.geojson",
+                                      "SatelliteSceneTiles/sentinel2_tiles_world/sentinel2_tiles_world.geojson")
+        # Create bool for intersection between any tiles - should try inversion to speed up...
+        world_granules[region] = world_granules.geometry.apply(lambda x: gpd.GeoSeries(x).intersects(aoi))
+        # Filter based on any True intersections
+        world_granules[region] = world_granules[world_granules[region]].any(1)
+        region_s2_grans = world_granules[world_granules[region] == True]
 
         # logger.info(f"Querying ESA for {len(region_s2_grans.index)} tiles")
         # res = self._esa_api.query(
         #     area=aoi,
         #     platformname='Sentinel-2',
-        #     producttype='S2MSI2A',
+        #     producttype='S2MSI1c',
         #     limit=1
         # )
         #
@@ -54,41 +56,39 @@ class S2(BaseWorkFinder):
         res2 = self._esa_api.query(
             area=aoi,
             platformname='Sentinel-2',
-            producttype='S2MSI1C',
-            limit=10
+            producttype='S2MSI1C'
         )
         esa_l1c = self._esa_api.to_geodataframe(res2)
-        # save to file before_processing.csv
-        esa_l1c.to_csv("before_processing.csv")
-
-        #
         esa_l1c['id'] = esa_l1c.title.apply(
             lambda x: f"{x.split('_')[0]}_{x.split('_')[1]}_{x.split('_')[2]}_{x.split('_')[5]}")
         # esa_l2a['scenename'] = esa_l2a.title.apply(
         #     lambda x: f"{x.split('_')[0]}_MSIL1C_{x.split('_')[2]}_{x.split('_')[5]}")
-        # esa_l1c['granules'] = esa_l1c.identifier.str[39:44]
+        esa_l1c['granules'] = esa_l1c.identifier.str[39:44]
         # esa_l2a['granules'] = esa_l2a.identifier.str[39:44]
         #
-        # esa_l1c_srt = esa_l1c.sort_values('beginposition', ascending=False)
+        esa_l1c_srt = esa_l1c.sort_values('beginposition', ascending=False)
         # esa_l2a_srt = esa_l2a.sort_values('beginposition', ascending=False)
         # esa_l1c_srt = esa_l1c_srt.loc[~esa_l1c_srt['scenename'].isin(esa_l2a_srt.scenename.values)]
         #
-        # esa_l1c_precise = esa_l1c_srt[esa_l1c_srt['granules'].isin(region_s2_grans.Name.values)]
+        esa_l1c_precise = esa_l1c_srt[esa_l1c_srt['granules'].isin(region_s2_grans.Name.values)]
         # esa_l2a_precise = esa_l2a_srt[esa_l2a_srt['granules'].isin(region_s2_grans.Name.values)]
         #
         # result = pd.concat([esa_l1c_precise['scenename'], esa_l2a_precise['scenename']], axis=0)
 
-        logger.info(f"Found {len(esa_l1c)} L1C scenes to process")
+        logger.info(f"Found {len(esa_l1c_precise)} L1C scenes to process")
         # save esa_l1c to .csv
         # rename scenename column to id
         #  esa_l1c.rename(columns={'scenename': 'id'}, inplace=True)
-        esa_l1c.to_csv("esa_l1c.csv")
-        logger.info(f"Type of ESAL1C : {type(esa_l1c)}")
-        # drop the geometry column
-        esa_l1c.drop(columns='geometry', inplace=True)
-        esa_l1c = pd.DataFrame(esa_l1c)
-        logger.info(f"Type of ESAL1C : {type(esa_l1c)}")
-        return esa_l1c
+        # drop the stuff that cant be jsonified
+        esa_l1c_precise.drop(columns='geometry', inplace=True)
+        esa_l1c_precise.drop(columns='datatakesensingstart', inplace=True)
+        esa_l1c_precise.drop(columns='generationdate', inplace=True)
+        esa_l1c_precise.drop(columns='beginposition', inplace=True)
+        esa_l1c_precise.drop(columns='endposition', inplace=True)
+        esa_l1c_precise.drop(columns='ingestiondate', inplace=True)
+        esa_l1c_precise = pd.DataFrame(esa_l1c_precise)
+        logger.info(f"Type of ESAL1C : {type(esa_l1c_precise)}")
+        return esa_l1c_precise
 
     def find_already_done_list(self):
         region = get_config("APP", "REGION")
@@ -96,27 +96,18 @@ class S2(BaseWorkFinder):
         return get_ard_list(self._s3, f"{imagery_path}/{region.lower()}/sentinel_2/")
 
     def submit_tasks(self, to_do_list: pd.DataFrame):
+        self._redis.connect()
         logger.info(f"Submitting {len(to_do_list)} tasks to redis")
         logger.info(f"Type of to_do_list: {type(to_do_list)}")
         logger.info(f"to_do_list: {to_do_list}")
         target_queue = get_config("S2", "REDIS_PROCESSED_CHANNEL")
 
         for index, row in to_do_list.iterrows():
-            # convert row to json
+            # convert row to dict
+            row_dict = row.to_dict()
+            self._redis.publish(target_queue, json.dumps(row_dict))
+        self._redis.close()
 
-            row_json = row.to_json()
-            self._redis.publish(target_queue, json.dumps(row_json))
-        exit(1)
-
-        #
-        # for r in to_do_list:
-        #     payload = {
-        #         "in_scene": r['url'],
-        #         "s3_bucket": target_bucket,
-        #         "s3_dir": f"{imagery_path}/{region.lower()}/sentinel_2/"
-        #     }
-        #     self._redis.publish(target_queue, json.dumps(payload))
-        #
 
 def _row_mapping(row):
     return {'id': row['scenename'], 'url': row['scenename']}
